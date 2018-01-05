ad0718eb7ee493fed39755ff32db679d838a847f
bundleofivan <bundleofivan@gmail.com>
2018-01-05 00:11:32 -0800

    Added Date and Day of Week
    Changed clock to 12-hour. Added day of week and date in top corners.

--- app/index.js
+++ app/index.js
@@ -8,15 +8,39 @@ clock.granularity = "minutes";
 
 // Get a handle on the <text> element
 let myLabel = document.getElementById("myLabel");
+let dayOfWeekLabel = document.getElementById("dayOfWeekLabel");
+let dateLabel = document.getElementById("dateLabel");
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var tick = 0;
 
 // Update the <text> element with the current time
 function updateClock() {
   let today = new Date();
-  let hours = today.getHours();
+  
+  var hours = today.getHours() % 12;
+  
+  if (hours == 0) {
+    hours = 12;
+  }
+  
   let mins = util.zeroPad(today.getMinutes());
 
+  let dayOfWeek = today.getDay();
+
+  let month = today.getMonth() + 1;
+  let day = today.getDate();
+  
   myLabel.text = `${hours}:${mins}`;
+  dayOfWeekLabel.text = days[dayOfWeek];
+  dateLabel.text = `${month}/${day}`;
+  
+  
 }
+
 
 // Update the clock every tick event
 clock.ontick = () => updateClock();
+
+// Next: update clock per second for colon
