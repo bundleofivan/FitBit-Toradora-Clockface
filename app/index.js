18cd715e4b5d502bfe368a87099f310d5adfdd5f
bundleofivan <bundleofivan@gmail.com>
2018-01-08 02:46:32 -0800

    Added Heart Rate Monitor and Display
    Enabled heart rate permissions in package.json file and displays heart rate. Heart rate currently updates every 2000 milliseconds.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 import clock from "clock";
 import document from "document";
+import { HeartRateSensor } from "heart-rate";
 
 import * as util from "../common/utils";
+
 
 // Update the clock every minute
 clock.granularity = "seconds";
@@ -10,13 +12,20 @@ clock.granularity = "seconds";
 let myLabel = document.getElementById("myLabel");
 let dayOfWeekLabel = document.getElementById("dayOfWeekLabel");
 let dateLabel = document.getElementById("dateLabel");
+let heartRateLabel = document.getElementById("heartRateLabel");
+heartRateLabel.text = "--";
 
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// Create a new instance of the HeartRateSensor object
+
+// Begin monitoring the sensor
+
 
 var tick = 0;
 
 // Update the <text> element with the current time
 function updateClock() {
+
   let today = new Date();
   
   var hours = today.getHours() % 12;
@@ -33,14 +42,10 @@ function updateClock() {
   let month = today.getMonth() + 1;
   let day = today.getDate();
   
-
-    myLabel.text = `${hours}:${mins}:${seconds}`;
-
-
+  myLabel.text = `${hours}:${mins}:${seconds}`;
 
   dayOfWeekLabel.text = days[dayOfWeek];
   dateLabel.text = `${month}/${day}`;
-  
   
 }
 
@@ -48,4 +53,21 @@ function updateClock() {
 // Update the clock every tick event
 clock.ontick = () => updateClock();
 
-// Next: update clock per second for colon
+var hrm = new HeartRateSensor();
+
+hrm.onreading = function() {
+
+  // Peek the current sensor values
+  console.log("Current heart rate: " + hrm.heartRate);
+  heartRateLabel.text = hrm.heartRate + " bpm";
+  // Stop monitoring the sensor
+  hrm.stop();
+}
+
+// updates heart rate every 2 seconds
+setInterval(updateHeartRate, 2000);
+
+function updateHeartRate() {
+  hrm.start();
+}
+
